727c5a6544227d0d406dbf5e31a42d977e55a622
Benjamin Deininger <Benjamin.M.Deininger.ctr@nga.mil>
2023-02-07 16:43:22 -0700

    feature

--- server.js
+++ server.js
@@ -7,11 +7,70 @@ const axios = require("axios")
 const app = express();
 const port = process.env.PORT || 3001;
 
-const WALLET_KEY = process.env.WALLET_KEY;
-const ETHER_API_KEY = process.env.ETHER_API_KEY;
-const ETHER_NETWORK = process.env.ETHER_NETWORK;
+//const WALLET_KEY = process.env.WALLET_KEY;
+//const ETHER_API_KEY = process.env.ETHER_API_KEY;
+//const ETHER_NETWORK = process.env.ETHER_NETWORK;
+const ETHER_NETWORK = 'mainnet';
+const API_KEY = '';
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'build')));
+
+app.post("/owned", async (req, res) => {
+    const body = req.body
+    if (!body) {
+        res.status(500).json({
+            message: "No post body"
+        })
+        return;
+    }
+
+    const address = body.address;
+    if (!address) {
+        res.status(500).json({
+            message: "No Address in post body"
+        })
+        return;
+    }
+
+    let tokens = await getOwnedTokens(address);
+
+    if (tokens) {
+        res.status(200).json(tokens)
+    } else {
+        res.status(500).json({ message : "Could not get owned tokens"})
+    }
+});
+
+const getOwnedTokens = async (address) => {
+    let network = ETHER_NETWORK === 'mainnet' ? '' : `-${ETHER_NETWORK}`;
+    const contractAddress = '0xAc5Aeb3b4Ac8797c2307320Ed00a84B869ab9333';
+    return axios.get(`https://api${network}.etherscan.io/api?module=account&action=tokennfttx&contractaddress=${contractAddress}&address=${address}&page=1&offset=100&startblock=0&endblock=27025780&sort=asc&apikey=${API_KEY}`).then(response => {
+        let responseData = response.data;
+        console.log(response);
+        let tokens = responseData.result;
+
+        let tokenId = [];
+        if (!tokens) {
+            return tokenId;
+        }
+        tokens.forEach(token => {
+            if (token.to === address) {
+                tokenId.push(token.tokenID);
+            }
+        })
+
+        tokens.forEach(token => {
+            if (token.from === address) {
+                tokenId.splice(tokenId.indexOf(token.tokenID), 1);
+            }
+        })
+
+        return tokenId;
+    }).catch(err => {
+        console.log(err);
+    });
+}
+
 app.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html')));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- src/Mint.js
+++ src/Mint.js
@@ -2,34 +2,94 @@ import React from "react";
 import "./App.css";
 import { Link } from "react-router-dom";
 import header from './img/header.png';
-
+import { abi, address, bytecode }  from  "../src/abi/og_contract.json"
+import {BigNumber, ethers} from "ethers"
 import banner from './img/banner.mp4';
+import metamask from './img/metamask.png';
 import comingSoon from './img/coming_soon_fence.png';
-
+import styled from "styled-components";
 
 class Mint extends React.Component {
 
-    async componentDidMount() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            accounts: undefined
+        };
+        this.connectMetamask = this.connectMetamask.bind(this);
+    }
 
+    async connectMetamask() {
+        const {ethereum} = window
+        let accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        this.setState({
+            accounts : accounts
+        })
+    }
+
+    async componentDidMount() {
+        const {ethereum} = window
+        if (ethereum) {
+            const accounts = await ethereum.request({
+                method: 'eth_accounts',
+            }).catch((error) => {
+                console.error(error);
+            });
+
+            if (!accounts || accounts.length === 0) {
+                return;
+            }
+
+            this.setState({
+                accounts: accounts,
+            })
+        }
     }
 
     render() {
 
+        const { accounts } = this.state;
+
         return (
             <div style={{backgroundSize: "cover", backgroundColor: "black", color: "lightgray", lineHeight: "1.8", letterSpacing: "1px"}}>
 
-                <div style={{textAlign: "center", marginTop: "10px"}}><Link to="/">
-                    <img src={header} style={{marginLeft: "10px", height: "75px", zIndex: 1, position: "absolute"}} alt="header"/></Link>
-                    <video style={{maxWidth: "100%"}} autoPlay playsInline loop muted>
+                <div style={{marginTop: "10px"}}><Link to="/">
+                    <img src={header} style={{marginLeft: "10px", height: "75px"}} alt="header"/></Link>
+{/*                    <video style={{maxWidth: "100%"}} autoPlay playsInline loop muted>
                         <source src={banner} type="video/mp4"/>
-                    </video>
+                    </video>*/}
                 </div>
 
-                <div style={{textAlign: "center"}}>
-                    <img src={comingSoon} style={{ marginTop: "10px", width: "50%"}} alt="comingSoon"/>
-                </div>
+                {!accounts || accounts.length === 0 ?
 
-                <div style={{textAlign: "center"}}>
+                    <div style={{textAlign: "center", marginTop: "20px"}}>
+                        <div>
+
+                        <div style={{ display: "flex", justifyContent: "center"}} onClick={() => this.connectMetamask()}>
+
+                            <div style={{ border: "1px dashed", padding: "10px", display: "flex", alignItems: "center"}}>
+                    
+
+                            <img src={metamask} style={{ height: "32px"}} alt="comingSoon"/>
+                                <span style={{  paddingLeft: "10px", height: "32px"}}>Connect with MetaMask</span>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    :
+                    <div style={{textAlign: "center"}}>
+                        <img src={comingSoon} style={{ marginTop: "10px", width: "50%"}} alt="comingSoon"/>
+                    </div>
+
+
+
+
+
+                }
+
+
+
+                <div style={{textAlign: "center", marginTop : "500px"}}>
                     90s kids © 2023
                 </div>
             </div>
