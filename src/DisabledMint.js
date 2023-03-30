485a3fba166cd99cffc2dc147f595eb331f4be22
Benjamin Deininger <bdeining@Benjamins-MacBook-Air.local>
2023-03-29 23:56:52 -0700

    Drop 2

--- src/DisabledMint.js
+++ src/DisabledMint.js
@@ -1,41 +1,56 @@
 import React from "react";
 import "./App.css";
 import { Link } from "react-router-dom";
-import header from './img/header.png';
+import header from "./img/header.png";
 
-import banner from './img/banner.mp4';
-import comingSoon from './img/coming_soon_fence.png';
-
+import banner from "./img/banner.mp4";
+import comingSoon from "./img/coming_soon_fence.png";
+import closed from "./img/closed_fence.png";
 
 class Mint extends React.Component {
+  async componentDidMount() {}
 
-    async componentDidMount() {
+  render() {
+    return (
+      <div
+        style={{
+          backgroundSize: "cover",
+          backgroundColor: "black",
+          color: "lightgray",
+          lineHeight: "1.8",
+          letterSpacing: "1px",
+        }}
+      >
+        <div style={{ textAlign: "center", marginTop: "10px" }}>
+          <Link to="/">
+            <img
+              src={header}
+              style={{
+                marginLeft: "10px",
+                height: "75px",
+                zIndex: 1,
+                position: "absolute",
+              }}
+              alt="header"
+            />
+          </Link>
+          <video style={{ maxWidth: "100%" }} autoPlay playsInline loop muted>
+            <source src={banner} type="video/mp4" />
+          </video>
+        </div>
 
-    }
+        <div style={{ textAlign: "center" }}>
+          <img
+            src={closed}
+            style={{ marginTop: "10px", width: "50%" }}
+            alt="closed"
+          />
+        </div>
 
-    render() {
-
-        return (
-            <div style={{backgroundSize: "cover", backgroundColor: "black", color: "lightgray", lineHeight: "1.8", letterSpacing: "1px"}}>
-
-                <div style={{textAlign: "center", marginTop: "10px"}}><Link to="/">
-                    <img src={header} style={{marginLeft: "10px", height: "75px", zIndex: 1, position: "absolute"}} alt="header"/></Link>
-                    <video style={{maxWidth: "100%"}} autoPlay playsInline loop muted>
-                        <source src={banner} type="video/mp4"/>
-                    </video>
-                </div>
-
-                <div style={{textAlign: "center"}}>
-                    <img src={comingSoon} style={{ marginTop: "10px", width: "50%"}} alt="comingSoon"/>
-                </div>
-
-                <div style={{textAlign: "center"}}>
-                    90s kids © 2023
-                </div>
-            </div>
-        );
-    }
+        <div style={{ textAlign: "center" }}>90s kids © 2023</div>
+      </div>
+    );
+  }
 }
-
 
 export default Mint;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import Layout from "./Layout";
 import About from "./About";
 import Shop from "./Shop";
-import Mint from "./DisabledMint";
+import Mint from "./Mint";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
