27048494d023e8f084b81ddc684938a41e93d9c1
Benjamin Deininger <bdeining@Benjamins-Air.hsd1.co.comcast.net>
2023-04-30 21:59:16 -0600

    Revert "Add flipbook functionality"
    This reverts commit af99e8727051c3b28df3ccad3cbbcf90c0603233.

--- public/assets/resources/shapes.js
+++
@@ -1,1 +0,0 @@
-window.shapes_tjs4i9hbdc = function() { return {"1":"<svg id=\"icon-1\" xmlns=\"http:\/\/www.w3.org\/2000\/svg\" viewBox=\"0 0 55 55\" stroke=\"none\" preserveAspectRatio=\"none\">\n    <path fill-rule=\"evenodd\" d=\"M0 0h55v55H0z\"\/>\n<\/svg>\n",};}
